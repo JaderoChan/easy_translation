17bd493a7c1bda5d85b1a24d01ca7a7cdb2fc808
JaderoChan <c_dl_cn@outlook.com>
2025-12-14 02:36:24 +0800

    remove the build_install_run scripts

--- example/base_example/build_install_run_mingw.sh
+++
@@ -1,7 +0,0 @@
-cmake -B build/mingw -G "MinGW Makefiles"
-cd build/mingw
-make -j
-cmake --install .
-cd install/bin
-chmod +x EasyTranslate-BaseExample
-EasyTranslate-BaseExample
--- example/base_example/build_install_run_msvc2022.bat
+++
@@ -1,6 +0,0 @@
-cmake -B build/vc2022 -G "Visual Studio 17 2022"
-cd build/vc2022
-msbuild EasyTranslate-BaseExample.sln /p:Configuration=Release /p:Platform=x64
-cmake --install .
-cd install/bin
-EasyTranslate-BaseExample
